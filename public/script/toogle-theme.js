b9e4761a31abdafcd0822e80cc60af9436fb0729
Zsolt Márku <zsolt.marku910221@gmail.com>
2023-04-28 20:49:53 +0200

    add day&night mode toogle function

---
+++ public/script/toogle-theme.js
@@ -0,0 +1,6 @@
+const toogleMode = document.getElementById("checkbox");
+
+checkbox.addEventListener("click", () => {
+    //a website temajanak megvaltoztatasa
+    document.body.classList.toggle("dark");
+});
