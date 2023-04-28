b04562fd9e7c1a173bef734753ab0a89e7b3d7c4
Zsolt Márku <zsolt.marku910221@gmail.com>
2023-04-28 07:25:35 +0200

    Level 5: Cookies and Sessions

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-// require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const port = 3000;
 const mongoose = require("mongoose");
-const md5 = require("md5");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
-// const encrypt = require("mongoose-encryption");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const app = express();
 
@@ -17,6 +16,16 @@ app.use(bodyParser.urlencoded({
 }));
 app.set('view engine', 'ejs');
 
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 5 * 60 * 1000 }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 mongoose.connect("mongodb://127.0.0.1:27017/usersDB");
 
 const userSchema = new mongoose.Schema({
@@ -24,12 +33,14 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-// userSchema.plugin(encrypt, {
-//     secret: process.env.SECRET,
-//     encryptedFields: ['password']
-// });
+userSchema.plugin(passportLocalMongoose);
 
 const User = mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", (req, res) => {
     res.render("home")
@@ -39,113 +50,53 @@ app.get("/login", (req, res) => {
     res.render("login")
 });
 
-app.post("/login", (req, res) => {
-    User.findOne({ email: req.body.username })
-        .then((foundUser) => {
-            if (!foundUser) {
-                console.log("No user found.");
-                res.render("error");
-            } else {
-                bcrypt.compare(req.body.password, foundUser.password)
-                    .then((result) => {
-                        if (result) {
-                            console.log("User was successfully logged in.");
-                            res.render("secrets");
-                        } else {
-                            console.log("Incorrect password.");
-                            res.render("error_password");
-                        }
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    });
-            }
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+app.get('/logout', (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        };
+        console.log("User succesfully logged out");
+        res.redirect('/');
+    });
 });
-
-//refactor above code with async/await and catch block to handle errors
-// app.post("/login", async (req, res) => {
-//     try {
-//       const foundUser = await User.findOne({ email: req.body.username });
-//       if (!foundUser) {
-//         console.log("No user found.");
-//         return res.render("error");
-//       }
-//       const result = await bcrypt.compare(req.body.password, foundUser.password);
-//       if (result) {
-//         console.log("User was successfully logged in.");
-//         return res.render("secrets");
-//       } else {
-//         console.log("Incorrect password.");
-//         return res.render("error_password");
-//       }
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   });
 
 app.get("/register", (req, res) => {
     res.render("register")
 });
-//with Promises
+
+app.get("/secrets", (req, res) => {
+    if (req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+});
+
+app.get("/error", (req, res) => {
+    res.render("error")
+})
+
 app.post("/register", (req, res) => {
-    bcrypt.hash(req.body.password, saltRounds)
-        .then((hash) => {
-            const newUser = new User({
-                email: req.body.username,
-                password: hash
-            })
-            User.findOne({ email: req.body.username })
-                .then((existingUser) => {
-                    if (existingUser) {
-                        console.log(`User already exist with email: ${req.body.username}`);
-                        res.render("exist")
-                    } else {
-                        newUser.save()
-                            .then(() => {
-                                console.log("Succesfully added a new user.");
-                                res.render("secrets");
-                            })
-                            .catch((err) => {
-                                console.log(err);
-                            });
-                    }
-                })
-                .catch((err) => {
-                    console.log(err);
-                });
+    User.register({ username: req.body.username }, req.body.password)
+        .then(() => {
+            passport.authenticate("local")(req, res, () => {
+                console.log("Successfully added a new user.");
+                res.redirect("/secrets");
+            });
         })
-        .catch((err) => {
-            console.log(err);
+        .catch(err => {
+            if (err.name === "UserExistsError") {
+                console.log(`User already exists with email: ${req.body.username}`);
+                res.render("exist");
+            }
         });
 });
 
-//refactor above code with async/await and catch block to handle errors
-// app.post("/register", async (req, res) => {
-//     try {
-//         const hash = await bcrypt.hash(req.body.password, saltRounds);
-//         const newUser = new User({
-//             email: req.body.username,
-//             password: hash
-//         });
-//         const existingUser = await User.findOne({ email: req.body.username });
-//         if (existingUser) {
-//             console.log(`User already exist with email: ${req.body.username}`);
-//             res.render("exist")
-//         } else {
-//             await newUser.save();
-//             console.log("Successfully added a new user.");
-//             res.render("secrets");
-//         }
-//     } catch (err) {
-//         console.log(err);
-//     }
-// });
+app.post("/login", passport.authenticate("local", {
+    successRedirect: "/secrets",
+    failureRedirect: "/error",
+}));
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
-})
-
+});
